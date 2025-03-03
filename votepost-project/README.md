a3649f59080577036d4659dbc67dcd595a990ee4
phignight <phignight@randolphcollege.edu>
2025-03-03 12:46:18 -0500

    Delete votepost-project directory

--- votepost-project/README.md
+++
@@ -1,1 +0,0 @@
-# votepost-project
--- votepost-project/public/votepage_script.js
+++
@@ -1,9 +0,0 @@
-function displayRadioValue() {
-    var ele = document.getElementsByName('gender');
-
-    for (i = 0; i < ele.length; i++) {
-        if (ele[i].checked)
-            document.getElementById("result").innerHTML
-                = "Gender: " + ele[i].value;
-    }
-}   
